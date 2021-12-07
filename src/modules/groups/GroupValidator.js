9fd4291da4939bee361c081c057fda99547aadc8
Vanja Karpuzovska <vanja.karpuzovska@aucta.dev>
2021-12-07 11:23:56 +0100

    validation changes

--- src/modules/groups/GroupValidator.js
+++ src/modules/groups/GroupValidator.js
@@ -2,8 +2,17 @@ import * as yup from "yup";
 
 export const CreateGroupValidator = yup.object().shape({
   name: yup.string().required("Име на групата е задолжително поле"),
+  code: yup
+    .string()
+    .required("Код за групата е задолжително поле")
+    .max(5, "Кодот мора да содржи најмногу 5 карактери")
+    .matches(/[A-Z]/, "Сите карактери мора да се запишани со голема буква"),
 });
 
 export const UpdateGroupValidator = yup.object().shape({
   name: yup.string().required("Име на групата е задолжително поле"),
+  code: yup
+    .string()
+    .required("Код за групата е задолжително поле")
+    .matches(/[A-Z]/, "Сите карактери мора да се запишани со голема буква"),
 });
