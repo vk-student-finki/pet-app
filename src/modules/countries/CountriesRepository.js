fa5bf055068761e7607f86d549e02be82704c216
Simona Anchova <62893553+simonaxanchova@users.noreply.github.com>
2021-12-21 14:18:28 +0100

    countries

--- src/modules/countries/CountriesRepository.js
+++ src/modules/countries/CountriesRepository.js
@@ -3,6 +3,7 @@ import { SETTINGS } from "../common/Settings";
 
 export const CountriesRepository = {
   all: (page, size, searchParams) => {
+    if (!searchParams) searchParams = {};
     return axios({
       url: `${SETTINGS.API_BASE_URL}countries`,
       method: "GET",
