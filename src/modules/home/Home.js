705bb41ec0a1598e8609329d7c720ae0e3c2e39a
Simona Anchova <62893553+simonaxanchova@users.noreply.github.com>
2021-12-22 16:49:18 +0100

    changes

--- src/modules/home/Home.js
+++ src/modules/home/Home.js
@@ -1,11 +1,31 @@
+import { Grid } from "@mui/material";
 import React from "react";
 
 export const Home = ({}) => {
   return (
     <>
-      <h1 style={{ fontFamily: "Helvetica, sans-serif", color: "#1F393C" }}>
-        Home page
-      </h1>
+      <Grid container>
+        <Grid item xs={12} style={{ background: "#f1f2f6", height: "150px" }}>
+          <Grid item xs={12} style={{ textAlign: "center" }}>
+            <Grid container>
+              <Grid item xs={12}>
+                <span
+                  style={{
+                    fontFamily: "Copperplate, fantasy",
+                    fontSize: "30px",
+                    color: "#1e1f1c",
+                    display: "block",
+                    paddingTop: "50px",
+                    textTransform: "uppercase",
+                  }}
+                >
+                  Welcome
+                </span>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   );
 };
