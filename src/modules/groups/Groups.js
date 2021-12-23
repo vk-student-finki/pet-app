c7c9896ba09b2c857687dda4a3616e4b5a87d33a
Simona Anchova <62893553+simonaxanchova@users.noreply.github.com>
2021-12-23 12:00:17 +0100

    delete group dialog added

--- src/modules/groups/Groups.js
+++ src/modules/groups/Groups.js
@@ -11,8 +11,13 @@ import {
   TableRow,
   Container,
   Stack,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Slide,
 } from "@mui/material";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { GroupsRepository } from "./GroupsRepository";
 import EditIcon from "@mui/icons-material/Edit";
@@ -20,12 +25,29 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import { COMMON_ACTIONS } from "../common/CommonActions";
 import { useDispatch } from "react-redux";
 import { AuthService } from "../auth/AuthService";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 export const Groups = () => {
   const [groups, setGroups] = useState();
   const [loading, setLoading] = useState(false);
   const [redirectTo, setRedirectTo] = useState();
   const dispatch = useDispatch();
+  const [selectedGroup, setSelectedGroup] = useState();
+
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = (group) => {
+    setSelectedGroup(group);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     loadData(0, 10);
@@ -200,9 +222,7 @@ export const Groups = () => {
                           color: "#D35400",
                           marginRight: "-15px",
                         }}
-                        onClick={() => {
-                          setRedirectTo(`/groups/delete/${group?.id}`);
-                        }}
+                        onClick={() => handleClickOpen(group)}
                       ></DeleteIcon>
                     </TableCell>
                   </TableRow>
@@ -230,6 +250,62 @@ export const Groups = () => {
           </Grid>
         </Grid>
       </Grid>
+
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <HighlightOffIcon
+          style={{
+            color: "#F15E5E",
+            marginLeft: "auto",
+            marginRight: "auto",
+            fontSize: "70px",
+            marginTop: "10px",
+          }}
+        />
+        <DialogTitle
+          style={{
+            fontWeight: "bold",
+            fontFamily: "Monaco, monospace",
+          }}
+        >
+          {"Confirm delete"}
+        </DialogTitle>
+        <DialogContent style={{ textAlign: "center" }}>
+          Are you sure you want to delete this group? This action
+          <br /> cannot be undone.
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handleClose}
+            variant="outlined"
+            size="large"
+            style={{
+              backgroundColor: "#C1C1C1",
+              color: "white",
+              border: "#C1C1C1",
+            }}
+          >
+            Cancel
+          </Button>
+          <Button
+            size="large"
+            style={{
+              backgroundColor: "#F15E5E",
+              color: "white",
+            }}
+            onClick={() => {
+              setRedirectTo(`/groups/delete/${selectedGroup?.id}`);
+            }}
+          >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 };
